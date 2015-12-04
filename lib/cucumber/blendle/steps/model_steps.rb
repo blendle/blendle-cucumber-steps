eb48f42d97f4c3b82f612413f04a27102ea92390
Jean Mertz <jean@mertz.fm>
2015-12-04 11:32:05 +0100

    allow value casting before saving to database

--- lib/cucumber/blendle/steps/model_steps.rb
+++ lib/cucumber/blendle/steps/model_steps.rb
@@ -1,27 +1,48 @@
 require 'active_support/core_ext/hash/keys'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/string/inflections'
+require 'sequel'
 
-# rubocop:disable Lint/Eval
-# rubocop:disable Lint/UnusedBlockArgument
+# rubocop:disable Lint/Eval,Metrics/LineLength,Lint/UselessAssignment
 
 Given(/^the following (\S+) exist:$/) do |object, table|
   table.hashes.each do |row|
-    assert eval("#{object.singularize.capitalize}.create(row.symbolize_keys)")
-    step %(the following #{object.singularize} should exist:), table([row.keys, row.values])
+    hash = parse_row(row)
+
+    assert eval("#{object.singularize.capitalize}.create(hash)")
+    step %(the following #{object.singularize} should exist:), table([hash.keys, hash.values])
   end
 end
 
-Then(/^the (\S+) with (\S+) "([^"]*)" should( not)? exist$/) do |object, attribute, negate, value|
+Then(/^the (\S+) with (\S+)(?: \((\S+)\))? "([^"]*)" should( not)? exist$/) do |object, attribute, type, value, negate|
+  hash      = parse_row("#{attribute} (#{type})" => value)
   assertion = negate ? 'blank?' : 'present?'
 
-  assert eval("#{object.capitalize}.first(attribute.to_sym => value).#{assertion}")
+  assert eval("#{object.capitalize}.first(hash).#{assertion}"),
+         %(#{object.capitalize} not found \(#{attribute}: #{value}\))
 end
 
 Then(/^the following (\S+) should( not)? exist:$/) do |object, negate, table|
   assertion = negate ? 'blank?' : 'present?'
 
   table.hashes.each do |row|
-    assert eval("#{object.capitalize}.first(row.symbolize_keys).#{assertion}")
+    hash = parse_row(row)
+
+    assert eval("#{object.capitalize}.first(hash).#{assertion}")
   end
+end
+
+def parse_row(row)
+  hash = row.map do |attribute, value|
+    value = case attribute[/\s\((\w+)\)$/, 1]
+            when 'Array'
+              Sequel.pg_array(eval(value))
+            else
+              value
+            end
+
+    [attribute.to_s.split.first.to_sym, value]
+  end
+
+  Hash[hash]
 end
