9daf70392872287e50b504ad9d7ccb0aff7af024
Jean Mertz <jean@mertz.fm>
2016-04-28 18:19:25 +0200

    release v0.8.2

--- lib/cucumber/blendle/version.rb
+++ lib/cucumber/blendle/version.rb
@@ -6,6 +6,6 @@ module Cucumber
   # This module defines default steps used by all of Blendle Ruby projects.
   #
   module BlendleSteps
-    VERSION = '0.8.1'.freeze
+    VERSION = '0.8.2'.freeze
   end
 end
