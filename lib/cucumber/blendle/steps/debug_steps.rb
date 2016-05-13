b0f7a7c71b186dea48218313d155e6630aa9ba62
Jean Mertz <jean@mertz.fm>
2016-05-13 07:57:38 +0200

    add debug step "I want to debug"

---
+++ lib/cucumber/blendle/steps/debug_steps.rb
@@ -0,0 +1,6 @@
+# * Then I want to pry
+# * Then I want to debug
+#
+Then(/^I want to (?:pry|debug)$/) do
+  require 'pry'; binding.pry # rubocop:disable Lint/Debugger,Style/Semicolon
+end
--- lib/cucumber/blendle_steps.rb
+++ lib/cucumber/blendle_steps.rb
@@ -8,6 +8,7 @@ require 'cucumber/blendle/support/minitest'
 require 'cucumber/blendle/support/rack_test'
 
 require 'cucumber/blendle/steps/benchmark_steps'
+require 'cucumber/blendle/steps/debug_steps'
 require 'cucumber/blendle/steps/mock_server_steps'
 require 'cucumber/blendle/steps/model_steps'
 require 'cucumber/blendle/steps/resource_steps'
